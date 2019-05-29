20f99bb3d9c4e5b5fafc0bab2a1b470b88ff8332
Paulo <phcs2610@hotmail.com>
2019-05-29 15:24:51 -0300

    Testes de Tópicos especiais em C# - Parte 1

---
+++ exemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a = DateTime.Parse("15:32 23/07/2015");
+            Console.WriteLine(a);
+        }
+    }
+}
